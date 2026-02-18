6b4c5df17f1fb1eeaabb39bece3e4419f95ada7d
sathish17130 <Sathish17130>
2026-02-18 11:33:16 +0530

    fix app registry error

--- core/apps.py
+++ core/apps.py
@@ -6,13 +6,3 @@ class CoreConfig(AppConfig):
     name = 'core'
 
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="admin123"
-    )
