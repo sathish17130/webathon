be1471f45c1ad355c8615829ce577369d24c4e7e
sathish17130 <Sathish17130>
2026-02-18 11:30:18 +0530

    create admin user

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="admin123"
+    )
